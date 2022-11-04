9549838fe52e1b5c057acc8ce7a55a52412643ab
iamagf <agf0710@gmail.com>
2022-11-04 16:31:03 -0600

    Part Seven - Instantiate Test with exercise

--- src/contract.rs
+++ src/contract.rs
@@ -46,4 +46,48 @@ pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use cosmwasm_std::attr; // constructs an attribute
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info}; // mock functions
+    use crate::contract::instantiate; // real contract instantiate function
+    use crate::msg::InstantiateMsg; // our instantiate method
+
+    // Fake addresses
+    pub const ADDR1: &str = "addr1";
+    pub const ADDR2: &str = "addr2";
+
+    #[test]
+    fn test_instantiate() {
+        let mut deps = mock_dependencies();
+        // Mock the contract environment (like a real chain)
+        let env = mock_env();
+        // Mock the message info, ADDR1 will be the sender, the empty vec means we sent no funds.
+        let info = mock_info(ADDR1, &vec![]);
+
+        // Create a message where the sender will be an admin
+        let msg = InstantiateMsg { admin: None };
+        // Call instantiate, unwrap to assert success
+        let res = instantiate(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(
+            res.attributes,
+            vec![attr("action", "instantiate"), attr("admin", ADDR1)]
+        )
+    }
+
+    #[test]
+    fn test_instantiate_with_admin() {
+        // Copy paste of test_instantiate but changes None to Some(ADDR2.to_string())
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info(ADDR2, &vec![]);
+
+        let msg = InstantiateMsg { admin: Some(ADDR2.to_string()) };
+        let res = instantiate(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(
+            res.attributes,
+            vec![attr("action", "instantiate"), attr("admin", ADDR2)]
+        )
+    }
+}
