616d93acc0fca69fda019a59fab4b6c753ec0bb3
iamagf <agf0710@gmail.com>
2022-11-04 18:47:34 -0600

    Part Eleven - Execute Tests

--- src/contract.rs
+++ src/contract.rs
@@ -60,7 +60,7 @@ fn execute_create_poll(
     options: Vec<String>,
 ) -> Result<Response, ContractError> {
     // Restricts # of options for creating the poll
-    if options.len() > 10 {
+    if options.len() > 5 {
         return Err(ContractError::TooManyOptions {});
     }
 
@@ -107,20 +107,19 @@ fn execute_vote(
                                 .position(|option| option.0 == ballot.option)
                                 .unwrap();
                             poll.options[position_of_old_vote].1 -= 1;
-                            Ok(Ballot { option: vote.clone() })
+                            Ok(Ballot {
+                                option: vote.clone(),
+                            })
                         }
-                        None => {
-                            Ok(Ballot { option: vote.clone() })
-                        }
+                        None => Ok(Ballot {
+                            option: vote.clone(),
+                        }),
                     }
                 },
             )?;
 
             // Find the position of the new vote option and increment it by 1
-            let position = poll
-                .options
-                .iter()
-                .position(|option| option.0 == vote);
+            let position = poll.options.iter().position(|option| option.0 == vote);
             if position.is_none() {
                 return Err(ContractError::Unauthorized {});
             }
@@ -130,17 +129,17 @@ fn execute_vote(
             // This stores the updated vote
             POLLS.save(deps.storage, poll_id, &poll)?;
             Ok(Response::new())
-        },
+        }
         // Poll doesn't exist
         None => Err(ContractError::PollNotFound {}), // Return error
     }
 }
 
-
 #[cfg(test)]
 mod tests {
-    use crate::contract::instantiate; // real contract instantiate function
-    use crate::msg::InstantiateMsg;
+    use crate::contract::{execute, instantiate}; // Adding execute
+    use crate::msg::{ExecuteMsg, InstantiateMsg}; // Adding ExecuteMsg
+    use crate::ContractError;
     use cosmwasm_std::attr; // constructs an attribute
     use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info}; // mock functions // our instantiate method
 
@@ -183,5 +182,132 @@ mod tests {
             res.attributes,
             vec![attr("action", "instantiate"), attr("admin", ADDR2)]
         )
+    }
+
+    // Start of new tests
+
+    // Poll valid tests
+    #[test]
+    fn test_execute_create_poll_valid() {
+        // The Mock values like in instantiate
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info(ADDR1, &vec![]);
+        // Instantiate the contract
+        let msg = InstantiateMsg { admin: None };
+        let _res = instantiate(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+
+        // New execute msg
+        let msg = ExecuteMsg::CreatePoll {
+            poll_id: "Web3Builders".to_string(),
+            question: "Will I finish this on time?".to_string(),
+            options: vec![
+                "Yes".to_string(),
+                "No".to_string(),
+                "The world will end before that".to_string(),
+            ],
+        };
+
+        // Unwrap to assert success
+        let _res = execute(deps.as_mut(), env, info, msg).unwrap();
+    }
+
+    //Poll invalid tests
+    #[test]
+    fn test_execute_create_poll_invalid() {
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info(ADDR1, &vec![]);
+        // Instantiate the contract
+        let msg = InstantiateMsg { admin: None };
+        let _res = instantiate(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+
+        let msg = ExecuteMsg::CreatePoll {
+            poll_id: "001".to_string(),
+            question: "How many numbers fit in this poll?".to_string(),
+            options: vec![
+                "1".to_string(),
+                "2".to_string(),
+                "3".to_string(),
+                "4".to_string(),
+                "5".to_string(),
+                "6".to_string(),
+            ],
+        };
+
+        let _err = execute(deps.as_mut(), env, info, msg).unwrap_err();
+    }
+
+    // Vote valid
+    #[test]
+    fn test_execute_vote_valid() {
+        // Envorinment
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info(ADDR1, &vec![]);
+
+        // Instantiate the contract
+        let msg = InstantiateMsg { admin: None };
+        let _res = instantiate(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+
+        // Poll created
+        let msg = ExecuteMsg::CreatePoll {
+            poll_id: "000".to_string(),
+            question: "Choose an option".to_string(),
+            options: vec!["1".to_string(), "2".to_string(), "3".to_string()],
+        };
+        let _res = execute(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+
+        // Vote executed
+        let msg = ExecuteMsg::Vote {
+            poll_id: "000".to_string(),
+            vote: "1".to_string(),
+        };
+        let _res = execute(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+
+        // Update vote
+        let msg = ExecuteMsg::Vote {
+            poll_id: "000".to_string(),
+            vote: "2".to_string(),
+        };
+        let _res = execute(deps.as_mut(), env, info, msg).unwrap();
+    }
+
+    // Vote invalid section
+    #[test]
+    fn test_execute_vote_invalid() {
+        let mut deps = mock_dependencies();
+        let env = mock_env();
+        let info = mock_info(ADDR1, &vec![]);
+        // Instantiate the contract
+        let msg = InstantiateMsg { admin: None };
+        let _res = instantiate(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+
+        // Vote created, poll doesn't exist.
+        let msg = ExecuteMsg::Vote {
+            poll_id: "000".to_string(),
+            vote: "Okonomiyaki".to_string(),
+        };
+        // Unwrap to assert error
+        let _err = execute(deps.as_mut(), env.clone(), info.clone(), msg).unwrap_err();
+
+        // Poll created
+        let msg = ExecuteMsg::CreatePoll {
+            poll_id: "000".to_string(),
+            question: "Favorite Japanese food".to_string(),
+            options: vec![
+                "Onigiri".to_string(),
+                "Okonomiyaki".to_string(),
+                "Ozoni".to_string(),
+            ],
+        };
+        let _res = execute(deps.as_mut(), env.clone(), info.clone(), msg).unwrap();
+
+        // Poll created, Vote done, option 
+        let msg = ExecuteMsg::Vote {
+            poll_id: "000".to_string(),
+            vote: "Pizza".to_string(),
+        };
+        let _err = execute(deps.as_mut(), env, info, msg).unwrap_err();
     }
 }
